196a4ec94c7c1accd402be9bb218ed995d934146
justi <justi@JustinRonaldo67>
2024-06-27 11:02:55 +0530

    this is my first  github project core java project

---
+++ javaBasic/src/java_Basic/java_basic_1.java
@@ -0,0 +1,15 @@
+package java_Basic;
+
+public class java_basic_1 {
+
+	public static void main(String[] args) {
+
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println("Add:"+c);
+		
+
+	}
+
+}
